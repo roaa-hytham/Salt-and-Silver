42d7e577c3157ebd1a766f855632f0de417ef447
roaa.elsaiid <roaa.hytham@gmail.com>
2025-11-03 22:50:22 +0200

    adjust player collider

--- Game-Prototype/Assets/Scripts/PlayerController.cs
+++ Game-Prototype/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed;
     private Animator anim;
     private Rigidbody rb;
+    private Vector3 movement;
+    private bool canMove = true;
 
     void Start()
     {
@@ -15,24 +18,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (!canMove) return;
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(v, 0f, h).normalized;
+        movement = new Vector3(v, 0f, h).normalized;
 
+        anim.SetBool("isWalking", movement.magnitude > 0.1f);
+    }
+    void FixedUpdate()
+    {
+        if (!canMove) return;
         if (movement.magnitude > 0.1f)
         {
-            anim.SetBool("isWalking", true);
-
-            Vector3 movePosition = rb.position + movement * speed * Time.deltaTime;
+            Vector3 movePosition = rb.position + movement * speed * Time.fixedDeltaTime;
             rb.MovePosition(movePosition);
-
             transform.forward = movement;
         }
-        else
-        {
-            anim.SetBool("isWalking", false);
-        }
     }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Wall"))
+    //    {
+    //        //canMove = false;
+    //        rb.linearVelocity = Vector3.zero;
+    //        movement = Vector3.zero;
+    //    }
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Wall"))
+    //    {
+    //        canMove = true;
+    //    }
+    //}
 
 }
