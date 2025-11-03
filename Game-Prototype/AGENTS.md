40754d92691a3ce95253eb0bdccc8037511f2f98
roaa.elsaiid <roaa.hytham@gmail.com>
2025-11-04 00:30:50 +0200

    Add cashRegister interaction with click

--- Game-Prototype/AGENTS.md
+++ Game-Prototype/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Game-Prototype
 - Unity version: Unity 6000.1.15f1
 - Active game object:
-  - Name: WallProps_SM_Chalkboard_6 (1)
-  - Tag: Wall
+  - Name: cashregister_cashdrawer
+  - Tag: CashRegister
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Game-Prototype/Assets/Scripts/InteractionController.cs
+++ Game-Prototype/Assets/Scripts/InteractionController.cs
@@ -1,43 +1,74 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class InteractionController : MonoBehaviour
 {
-    public float interactDistance = 3f;
+    public Camera cam;
+    public float rayDistance = 100f;
 
-    public string interactTrigger = "pressBtn";
+    public Animator anim;
 
-    private Animator playerAnim;
-    private Camera mainCam;
+    //public GameObject cashierRegister;
 
-    void Start()
+    private void Start()
     {
-        playerAnim = GetComponent<Animator>();
-        mainCam = Camera.main;
-
-        if (mainCam == null)
-            Debug.LogError("No Main Camera found in the scene. Tag your player camera as 'MainCamera'.");
+        anim = GetComponent<Animator>();
+        //powderPile.SetActive(false);
+        //creamContainer.SetActive(false);
     }
-
     void Update()
     {
-        if (mainCam == null) return;
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0)) // left-click
         {
-            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, interactDistance))
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, rayDistance))
             {
-                if (hit.collider.CompareTag("CashRegister"))
+                switch (hit.collider.tag)
                 {
-                    if (playerAnim != null)
-                        playerAnim.SetTrigger(interactTrigger);
+                    case "CashRegister":
+                        Debug.Log("Clicked on CashRegister");
+                        StartCoroutine(playPressBtnAnim());
+                        break;
 
-                    //Animator objAnim = hit.collider.GetComponent<Animator>();
-                    //if (objAnim != null)
-                    //    objAnim.SetTrigger(interactTrigger);
-
-                    Debug.Log("Interacted with: " + hit.collider.name);
+                    //case "grind":
+                    //    Debug.Log("Clicked on mortar and pestel");
+                    //    StartCoroutine(playGrindAnim());
+                    //    break;
+                    //case "potions":
+                    //    Debug.Log("Clicked on potions");
+                    //    powderPile.SetActive(false);
+                    //    creamContainer.SetActive(true);
+                    //    break;
+                    //case "remedy":
+                    //    Debug.Log("Clicked on remedy");
+                    //    creamContainer.SetActive(false);
+                    //    playerscript.addToInv(hit.collider.gameObject);
+                    //    break;
+                    default:
+                        Debug.Log("Clicked on something else: " + hit.collider.tag);
+                        break;
                 }
+
+                //if (hit.collider.tag == "plantPaper")
+                //{
+                //    GameObject tmp = playerscript.selectedItem;
+                //    GameObject obj = Instantiate(tmp, new Vector3(-6.8f, 1.56f, -6.4f), Quaternion.identity);
+                //    obj.SetActive(true);
+                //    obj.GetComponent<BoxCollider>().enabled = false;
+                //    obj.transform.localScale = new Vector3(tmp.transform.localScale.x / 2,
+                //                                           tmp.transform.localScale.y / 2,
+                //                                           tmp.transform.localScale.z / 2);
+                //}
             }
         }
     }
+
+    IEnumerator playPressBtnAnim()
+    {
+        anim.SetBool("pressBtn", true);
+        yield return new WaitForSeconds(3);
+        anim.SetBool("pressBtn", false);
+    }
+
 }
