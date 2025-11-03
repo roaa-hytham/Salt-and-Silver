63b9d39803ac0633532782ae8f55cd1283f4b94b
roaa.elsaiid <roaa.hytham@gmail.com>
2025-11-04 00:48:57 +0200

    add food interaction
    but donuts colliders are small so it doesn't really work yet

--- Game-Prototype/Assets/Scripts/InteractionController.cs
+++ Game-Prototype/Assets/Scripts/InteractionController.cs
@@ -6,16 +6,11 @@ public class InteractionController : MonoBehaviour
 {
     public Camera cam;
     public float rayDistance = 100f;
-
-    public Animator anim;
-
-    //public GameObject cashierRegister;
+    private Animator anim;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        //powderPile.SetActive(false);
-        //creamContainer.SetActive(false);
     }
     void Update()
     {
@@ -31,35 +26,15 @@ public class InteractionController : MonoBehaviour
                         StartCoroutine(playPressBtnAnim());
                         break;
 
-                    //case "grind":
-                    //    Debug.Log("Clicked on mortar and pestel");
-                    //    StartCoroutine(playGrindAnim());
-                    //    break;
-                    //case "potions":
-                    //    Debug.Log("Clicked on potions");
-                    //    powderPile.SetActive(false);
-                    //    creamContainer.SetActive(true);
-                    //    break;
-                    //case "remedy":
-                    //    Debug.Log("Clicked on remedy");
-                    //    creamContainer.SetActive(false);
-                    //    playerscript.addToInv(hit.collider.gameObject);
-                    //    break;
+                    case "Food":
+                        Debug.Log("Clicked on Food");
+                        hit.collider.gameObject.SetActive(false);
+                        break;
+
                     default:
                         Debug.Log("Clicked on something else: " + hit.collider.tag);
                         break;
                 }
-
-                //if (hit.collider.tag == "plantPaper")
-                //{
-                //    GameObject tmp = playerscript.selectedItem;
-                //    GameObject obj = Instantiate(tmp, new Vector3(-6.8f, 1.56f, -6.4f), Quaternion.identity);
-                //    obj.SetActive(true);
-                //    obj.GetComponent<BoxCollider>().enabled = false;
-                //    obj.transform.localScale = new Vector3(tmp.transform.localScale.x / 2,
-                //                                           tmp.transform.localScale.y / 2,
-                //                                           tmp.transform.localScale.z / 2);
-                //}
             }
         }
     }
