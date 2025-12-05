bdbab9f09a633363ab3d531f62057da5d0e9b587
roaa.elsaiid <roaa.hytham@gmail.com>
2025-12-06 01:13:38 +0200

    Add Market Materials

--- Game-Prototype/Assets/Scripts/MarketTransitionController.cs
+++ Game-Prototype/Assets/Scripts/MarketTransitionController.cs
@@ -5,12 +5,17 @@ public class MarketTransitionController : MonoBehaviour
 {
    private void OnTriggerEnter(Collider collider)
     {
+        // If player collides with ForestTrigger
         if (collider.CompareTag("ForestTrigger"))
         {
+            // Load the ForestScene
             SceneManager.LoadScene("ForestScene");
         }
+
+        // If player collides with RestaurantTrigger
         if (collider.CompareTag("RestaurantTrigger"))
         {
+            // Load the RestaurantScene
             SceneManager.LoadScene("RestaurantScene");
         }
     }
