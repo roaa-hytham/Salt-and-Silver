8b366d7bd3033dc8b4f730048b7a26c7311c1e25
roaa.elsaiid <roaa.hytham@gmail.com>
2025-12-06 00:50:30 +0200

    Add SavingScene

--- Game-Prototype/Assets/Scripts/ExitBtnController.cs
+++ Game-Prototype/Assets/Scripts/ExitBtnController.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ExitBtnController : MonoBehaviour
 {
-    public void ExitGame()
+    void Start()
     {
+        // Start the coroutine
+        StartCoroutine(ExitGameAfterSaving());
+
+        // Exit game
         Debug.Log("Exit game called.");
         Application.Quit();
+    }
+
+    IEnumerator ExitGameAfterSaving()
+    {
+        // Load the next scene
+        SceneManager.LoadScene("SavingScene");
+
+        // Wait for few seconds
+        yield return new WaitForSeconds(3f);
     }
 }
--- Game-Prototype/Assets/Scripts/StartBtnController.cs
+++ Game-Prototype/Assets/Scripts/StartBtnController.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class StartBtnController : MonoBehaviour
 {
-    public void StartGame()
+    void Start()
     {
+        SceneManager.LoadScene("LoadingScene");
+
+        // Start the coroutine
+        StartCoroutine(LoadNextSceneAfterDelay());
+    }
+
+    IEnumerator LoadNextSceneAfterDelay()
+    {
+        // Wait for few seconds
+        yield return new WaitForSeconds(3f);
+
+        // Load the next scene
         SceneManager.LoadScene("RestaurantScene");
     }
 }
