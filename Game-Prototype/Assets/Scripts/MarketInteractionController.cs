3402c7ffe8aa9a36c36ce5988d53636a7f28eb88
roaa.elsaiid <roaa.hytham@gmail.com>
2025-12-07 18:52:11 +0200

    Add restaurant open sign

---
+++ Game-Prototype/Assets/Scripts/MarketInteractionController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MarketInteractionController : MonoBehaviour
+{
+    public Camera cam;
+    public float rayDistance = 100f;
+    private Animator anim;
+    public PlayerData playerData;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // left-click
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, rayDistance))
+            {
+                switch (hit.collider.tag)
+                {
+                    case "RedMushroom":
+                        Debug.Log("Clicked on RedMushroom");
+                        StartCoroutine(playPickingUpAnim());
+                        hit.collider.gameObject.SetActive(false);
+                        playerData.redMushroomsCollected++;
+                        break;
+                    case "YellowMushroom":
+                        Debug.Log("Clicked on YellowMushroom");
+                        StartCoroutine(playPickingUpAnim());
+                        hit.collider.gameObject.SetActive(false);
+                        playerData.yellowMushroomsCollected++;
+                        break;
+                    case "SpiderLilies":
+                        Debug.Log("Clicked on SpiderLilies");
+                        StartCoroutine(playPickingUpAnim());
+                        hit.collider.gameObject.SetActive(false);
+                        playerData.spiderLiliesCollected++;
+                        break;
+                    default:
+                        Debug.Log("Clicked on something else: " + hit.collider.tag);
+                        break;
+                }
+            }
+        }
+    }
+
+    IEnumerator playPickingUpAnim()
+    {
+        anim.SetBool("pickingUp", true);
+        yield return new WaitForSeconds(2);
+        anim.SetBool("pickingUp", false);
+    }
+}
